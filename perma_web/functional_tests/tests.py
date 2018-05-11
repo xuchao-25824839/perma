7649d238a7479ffae82a8ba469bff2ffc85ba51a
Rebecca Cremona <rcremona@law.harvard.edu>
2018-05-11 16:26:44 -0400

    syntax

--- perma_web/functional_tests/tests.py
+++ perma_web/functional_tests/tests.py
@@ -129,7 +129,7 @@ def timing(f):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        print '%s function took %0.3f ms' % (f.func_name, (time2-time1)*1000.0)
+        print("{} function took {} ms" .format(f.func_name, (time2-time1)*1000.0))
         return ret
     return wrap
 
